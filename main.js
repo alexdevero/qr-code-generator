132c15a92bdc7ccf50210c216c0d0e635453a2f0
Alex Devero <deveroalex@gmail.com>
2020-12-24 13:44:15 +0100

    Change location of index.html

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./src/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -26,7 +26,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
