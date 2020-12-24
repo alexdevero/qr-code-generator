4b38bfda97036a34670f2d9ed4a465a4551a5e76
Alex Devero <deveroalex@gmail.com>
2020-12-24 14:53:52 +0100

    Switch to exposed QRCode dep

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -5,14 +5,12 @@
 // selectively enable features needed in the rendering
 // process.
 
-const qrcode = require('qrcode')
-
 const canvas = document.querySelector('#canvas')
 const textarea = document.querySelector('#textarea')
 const btn = document.querySelector('#btn')
 
 function generateQRCode() {
-  qrcode.toCanvas(canvas, textarea.value)
+  window.QRCode.toCanvas(canvas, textarea.value)
     .then((url) => console.log(url))
     .catch(err => console.log(err))
 }
